b335128c8435958118af3ab18aa4cfe9115fdca1
Abdul Waqas <abdulwaqas000@gmail.com>
2024-07-12 02:47:54 +0500

    grid 8 blue

--- src/main.cpp
+++ src/main.cpp
@@ -1345,6 +1345,215 @@ void grid3blue()
 
 }
 
+
+void grid7Blue(){
+  // blue
+  // 0,1,0
+  // 0,1,1
+  // 0,0,0
+  
+  //rock 1 drop  
+  lineFollowUntil(4,true,false);
+  checkPoint();
+  leftEncoder(210);
+  halt();
+  delay(500);
+  lineFollowUntil(4,true,false);
+  checkPoint();
+  leftEncoder(220);
+  halt();
+  delay(500);
+  lineFollowEncoderFront(40);
+  halt();
+  lineFollowEncoderBack(170);
+  treePicInit();
+  checkPoint();
+  lineFollowEncoderFront(160);
+  halt();
+  delay(500);
+  treePic();
+  delay(500);
+  centered();
+  rightEncoder(210);
+  halt();
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+  delay(500);
+  lineFollowEncoderFront(145);
+  halt();
+  delay(500);
+  treeDrop();
+
+  //  tree drop 1
+
+  delay(500);
+  lineFollowUntil(2,false,true);
+  centered();
+  rightEncoder(235);
+  delay(500);
+  lineFollowUntil(1,false,true);
+  delay(500);
+  centered();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  delay(500);
+  lineFollowEncoderFront(145);
+  delay(500);
+  treePic();
+  delay(500);
+  lineFollowUntil(3,false,true);
+  centered();
+  delay(500);
+  lineFollowEncoderBack(160);
+  delay(500);
+  treeDrop();
+  delay(500);
+
+  // rock 2 drop 
+  lineFollowUntil(1,false,true);
+  centered();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(1,false,true);
+  centered();
+  rightEncoder(230);
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+  delay(500);
+  leftEncoder(235);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treePic();
+  delay(500);
+  centered();
+
+  rightEncoder(230);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treeDrop();
+
+  /////tree 2 drop
+  delay(500);
+  lineFollowUntil(2,false,true);
+  centered();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  rightEncoder(230);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treePic();
+  delay(500);
+  centered();
+  leftEncoder(470 ,80);
+  delay(500);
+  lineFollowUntil(1,false,true);
+  delay(500);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treeDrop();
+  delay(500);
+  
+  //rock 3 drop
+
+  centered();
+  rightEncoder(230);
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treePic();
+  delay(500);
+  centered();
+  leftEncoder(230);
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+  rightEncoder(235);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  leftEncoder(235);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  lineFollowEncoderBack(160);
+  delay(500);
+  treeDrop();
+  delay(500);
+
+
+  // tree 3 drop 
+
+  centered();
+  leftEncoder(235);
+  delay(500);
+  lineFollowUntil(1,true,false);
+  checkPoint();
+  rightEncoder(235);
+  delay(500);
+  lineFollowUntil(1,false,true);
+  delay(500);
+  checkPoint();
+  delay(500);
+  lineFollowEncoderFront(145);
+  delay(500);
+  treePic();
+  delay(500);
+  lineFollowUntil(3,false,true);
+  centered();
+  rightEncoder(235);
+  delay(500);
+  lineFollowUntil(1,false,true);
+  delay(500);
+  checkPoint();
+  lineFollowEncoderFront(145);
+  delay(500);
+  treeDrop();
+  delay(500);
+  centered();
+  myservo.write(75);
+  leftEncoder(235);
+  delay(500);
+  lineFollowUntil(2,true,false);
+  checkPoint();
+//parking 
+  delay(500);
+  rightEncoder(235);
+  delay(500);
+  lineFollowUntil(4,true,false);
+  delay(500);
+  checkPoint();
+  lineFollowEncoderFront(300);
+
+}
+
+
+
 void setup() {
   Serial.begin(9600);
 
@@ -1403,7 +1612,7 @@ void setup() {
   // halt();
 
 
-  grid2Blue();
+  grid7Blue();
   // delay(1000);
   // leftEncoder(490,70);
   // myservo.write(12);
